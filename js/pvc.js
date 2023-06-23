8a040eca178808046d901a89ef2bb9cc8719b0c5
ghifarit53 <mghifarit53+github@gmail.com>
2023-06-23 17:14:24 +0700

    convert to static site

--- js/pvc.js
+++ js/pvc.js
@@ -14,7 +14,7 @@ Things that can be done :
 */
 
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import * as Entity from "./entities.js";
 
